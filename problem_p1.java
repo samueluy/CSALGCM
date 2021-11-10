a7ecd9825e16d76c46bc26c5f0414b584c4fe6b2
Samuel Uy <74288590+samueluy@users.noreply.github.com>
2021-11-10 15:33:43 +0800

    Create problem_p1.java

---
+++ problem_p1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.ArrayList;
+public class problem_p1 {
+    public void program_run(ArrayList<String> input) {
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        ArrayList<String>  input = new ArrayList();
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt(); // Ask for n value
+
+        for(int i=0; i<n; i++){
+            input.add(in.nextLine());
+        }
+
+      //  program_run(input);
+
+        }
+}
