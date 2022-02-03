c74ae3da6cd386ba974aa00fe9a69a406d949a33
Samuel Uy <74288590+samueluy@users.noreply.github.com>
2022-02-03 13:43:32 +0800

    Update problem_p3.java

--- src/problem_p3.java
+++ src/problem_p3.java
@@ -1,46 +1,51 @@
+/*
+https://www.geeksforgeeks.org/longest-common-subsequence-dp-4/
+https://www.programiz.com/dsa/longest-common-subsequence
+ */
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
-import java.util.Hashtable;
 
-public class problem_p3 {
-    static int counter=0;
+public class P3 {
 
-    Hashtable<Integer, Integer> convert (ArrayList<String> input){
-        Hashtable<Integer, Integer> values = new Hashtable<>();
-        String[] initial;
-        initial = input.get(1).split("\\s+"); // get train order
+    void printLCS(String X, String Y) {
+        int lenX = X.length();
+        int lenY = Y.length();
+        int[][] matrix = new int[lenX + 1][lenY + 1];
 
-        for(int i=1; i<=Integer.parseInt(input.get(0)); i++) {
-            values.put(i, Integer.parseInt(initial[i-1]));
-        }
-        return values;
-    }
-
-    void insertionSort(ArrayList<String> input){
-        Hashtable<Integer, Integer> values;
-        values = convert(input);
-
-        for(int i=2; i<=Integer.parseInt(input.get(0)); i++){
-            int holder = values.get(i);
-            int y = i-1;
-            while(y>0 && holder < values.get(y)){
-                values.put(y+1, values.get(y));
-                y--;
-                counter++;
+        for (int i = 0; i < lenX + 1; i++) {
+            for (int j = 0; j < lenY + 1; j++) {
+                if (i == 0 || j == 0) matrix[i][j] = 0;
+                else if (X.charAt(i - 1) == Y.charAt(j - 1)) matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                else matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i][j - 1]);
             }
-            values.put(y+1, holder);
         }
+        ArrayList<Character> result = matrixBacktrack(matrix, X, Y, X.length(), Y.length());
+        Collections.reverse(result); // reverse array list
+        for (Character character : result) System.out.print(character);
     }
 
-    public static void main(String[] args){
-        problem_p3 P3 = new problem_p3();
-        ArrayList<String>  input = new ArrayList();
+    ArrayList<Character> matrixBacktrack(int[][] matrix, String str1, String str2, int i, int j) {
+        ArrayList<Character> result = new ArrayList<>();
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                result.add(str1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (matrix[i - 1][j] > matrix[i][j - 1]) i--;
+            else j--;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        P3 problem_p2 = new P3();
         Scanner in = new Scanner(System.in);
 
-        for(int i=0; i<2; i++)
-            input.add(in.nextLine());
+        String X = in.nextLine();
+        String Y = in.nextLine();
 
-        P3.insertionSort(input);
-        System.out.println("The most optimal train swaps: " +  counter);
+        problem_p2.printLCS(X, Y);
     }
 }
