98faef4a1d5638748ebd6020cbc28197c2e4f2d7
Samuel Uy <74288590+samueluy@users.noreply.github.com>
2022-02-05 21:25:48 +0800

    Update problem_p1.java

--- src/problem_p1.java
+++ src/problem_p1.java
@@ -1,65 +1,116 @@
-import java.util.Scanner;
-import java.util.Stack;
+/*
+https://www.geeksforgeeks.org/java-program-for-matrix-chain-multiplication-dp-8/
+https://medium.com/javarevisited/implementation-of-matrix-chain-multiplication-using-dynamic-programming-in-java-e8828c1e9f9b
+https://www.geeksforgeeks.org/printing-brackets-matrix-chain-multiplication-problem/
+*/
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class problem_p1 {
-    private void program_run(ArrayList<String> input) {
-        Stack<Character> values = new Stack(); // Declare stack data structure
-        String item;
-
-        for(int i=1; i<input.size(); i++){ // itterate through all values in array list
-            boolean valid = true;
-            item = input.get(i);
-            for(int y=0; y<item.length(); y++) { //itterate through all characters of the string
-                char ch = item.charAt(y);
-                if(ch == '(' || ch == '[')
-                    values.push(item.charAt(y)); // push opening parenthesis or bracket to stack
-                else if(values.empty()){ // terminate if current stack is empty
-                    y=item.length();
-                    valid = false;
-                }
-                else if(ch == ')') {
-                    if (values.pop() != '(') {
-                        y = item.length(); // Terminate loop
-                        valid = false;
-                    }
-                }
-                    else if(ch == ')') {
-                        if (values.pop() != '(') {
-                            y = item.length(); // Terminate loop
-                            valid = false;
-                        }
-                    }
-                    else if (ch == ']') {
-                            if (values.pop() != '[') {
-                                y = item.length(); // Terminate loop
-                                valid = false;
-                            }
-                    }
-                    else{
-                        System.out.println("Invalid input");
-                        valid=false;
-                        y=item.length();
-                }
-
-            }
-            if(values.empty() && valid)  // Check if current stack is empty and valid
-                System.out.println("Yes");
-            else System.out.println("No");
-            values.clear();
+public class P1 {
+    void assignTable(ArrayList<String> inputs){
+        ArrayList<Integer> original = new ArrayList<>();
+        ArrayList<Character> letters = new ArrayList<>();
+        for (int i = 0; i < inputs.size(); i++) { // Store the values on each array
+            String[] temp = inputs.get(i).split("\\s");
+            letters.add(temp[0].charAt(0)); // Store the symbol
+            original.add(Integer.parseInt(temp[1])); // Store X
+            original.add(Integer.parseInt(temp[2])); // Store Y
         }
-        input.clear(); // Clear stack
+
+        ArrayList<Integer> nums = properDim(original);
+        if(nums.get(0) == -1){ // Input error check
+            System.out.println("Invalid matrix");
+            return;
+        }
+        int numsSize = nums.size();
+        int[][] matrix = new int[numsSize][numsSize];
+        int[][] parSequence = new int[numsSize][numsSize];
+
+
+        for(int i = 1; i < numsSize; i++)
+            matrix[i][i] = 0;
+        // L is chain length.
+        for(int L = 2; L < numsSize; L++)
+        {
+            for(int i = 1; i < numsSize - L + 1; i++)
+            {
+                int j = i + L - 1;
+                matrix[i][j] = Integer.MAX_VALUE;
+                for (int k = i; k <= j - 1; k++)
+                {
+
+                    // q = cost/scalar multiplications
+                    int q = matrix[i][k] + matrix[k + 1][j] +
+                        nums.get(i-1) * nums.get(k) * nums.get(j);
+
+                    if (q < matrix[i][j])
+                    {
+                        matrix[i][j] = q;
+
+                        // Each entry bracket[i,j]=k shows
+                        // where to split the product arr
+                        // i,i+1....j for the minimum cost.
+                        parSequence[i][j] = k;
+                    }
+                }
+            }
+        }
+
+        ArrayList<Character> parenthesisFormat = new ArrayList<>();
+        int parCount = -1;
+        parFormat(1, numsSize-1, parSequence, letters, parenthesisFormat, parCount);
+        for (Character character : parenthesisFormat) System.out.print(character);
+
     }
 
-    public static void main(String[] args){
-        problem_p1 P1 = new problem_p1();
-        ArrayList<String>  input = new ArrayList();
+    void parFormat(int i, int j,
+                                 int[][] matrix, ArrayList<Character> letters, ArrayList<Character> parFinal, int parCounter)
+    {
+        // If only one matrix left in current segment
+        if (i == j)
+        {
+            parFinal.add(letters.get(0));
+            letters.remove(0);
+            return;
+        }
+        parCounter++;
+        if(parCounter!=0)
+            parFinal.add('(');
+        parFormat(i, matrix[i][j], matrix, letters, parFinal, parCounter);
+
+        parFormat(matrix[i][j] + 1, j, matrix,letters, parFinal, parCounter);
+        if(parCounter!=0)
+            parFinal.add(')');
+    }
+
+    ArrayList<Integer> properDim(ArrayList<Integer> original){
+        ArrayList<Integer> trimmed = new ArrayList<>();
+            trimmed.add(original.get(0));
+            for(int i = 1; i < original.size()-1; i++){
+                if(original.get(i) == original.get(i+1)){
+                    trimmed.add(original.get(i));
+                    i++; // Skip to next number
+                }
+                else
+                    trimmed.add(0, -1); // Add error check value at the start
+            }
+            trimmed.add(original.get(original.size()-1));
+        return trimmed;
+    }
+    // Driver code
+    public static void main(String[] args) {
+        P1 problem_p1 = new P1();
+        ArrayList<String> inputs = new ArrayList<>();
         Scanner in = new Scanner(System.in);
 
-        input.add(in.nextLine());
-        for(int i=0; i<Integer.parseInt(input.get(0)); i++)
-            input.add(in.nextLine());
+        inputs.add(in.nextLine()); // First input
+        int size = Integer.parseInt(inputs.get(0));
+        inputs.clear(); // Empty
+        for (int i = 0; i < size; i++)
+            inputs.add(in.nextLine());
 
-        P1.program_run(input);
+        problem_p1.assignTable(inputs);
     }
 }
